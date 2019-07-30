1259a267f2dd2cd70c7a2179ff03f01d525801fe
xrr8417403 <279484148@qq.com>
2019-07-30 23:52:28 +0800

    home updage

--- Iciba/spiders/Iciba_spider.py
+++ Iciba/spiders/Iciba_spider.py
@@ -23,7 +23,8 @@ class IcibaSpider(scrapy.Spider):
                     class_id = sub_category_url.css("::attr(class_id)").extract_first()
                     url = "http://word.iciba.com/?action=courses&classid=" + str(class_id)
                     #print("<<%s>>%s=======================>%s,url:%s" %(header,sub_header,count,url))
-                    yield response.follow(url, callback=self.parse_chapter_url)
+                    yield response.follow(url, callback=self.parse_chapter_url,meta={"header":header,"sub_header":sub_header,\
+                        "count":count})
             else :
                 header = category_url.css("h3::text").extract_first()
                 sub_header = ""
@@ -31,19 +32,29 @@ class IcibaSpider(scrapy.Spider):
                 class_id = category_url.css("::attr(class_id)").extract_first()
                 url = "http://word.iciba.com/?action=courses&classid=" + str(class_id)
                 #print("%s=======================>%s,url::%s"%(header,count,url))
-                yield response.follow(url, callback=self.parse_chapter_url)
+                yield response.follow(url, callback=self.parse_chapter_url,meta={"header":header,"sub_header":sub_header,\
+                        "count":count})
 
     def parse_chapter_url(self,response):
         for chapter_url in response.css("ul.study-speed-m li.c_panel"):
+            header = response.meta["header"]
+            sub_header = response.meta["sub_header"]
+            count = response.meta["count"]
             class_id = response.request.url.split('=')[-1]
             course_id = chapter_url.css("::attr(course_id)").extract_first()
             word_url = "http://word.iciba.com/?action=words&class=" + str(class_id) + "&course=" + str(course_id)
-            print(word_url)
-            yield response.follow(word_url, callback=self.parse_word)
+            #print(word_url)
+            yield response.follow(word_url, callback=self.parse_word,meta={"header":header,"sub_header":sub_header,\
+                        "count":count})
 
     def parse_word(self,response):
         for info in response.css("ul.word_main_list li"):
-            word = info.css("div.word_main_list_w span::text").extract_first().strip()
-            phonetic = info.css("div.word_main_list_y strong::text").extract_first().strip()
-            chinese = info.css("div.word_main_list_s span::text").extract_first().strip()
-            print(word,phonetic,chinese)
+            yield{
+            'header':response.meta["header"],
+            'sub_header':response.meta["sub_header"],
+            'count':response.meta["count"],
+            'word':info.css("div.word_main_list_w span::text").extract_first().strip(),
+            'phonetic':info.css("div.word_main_list_y strong::text").extract_first().strip(),
+            'chinese':info.css("div.word_main_list_s span::attr(title)").extract_first().strip()
+            #print(header,sub_header,count,word,phonetic,chinese)
+            }
