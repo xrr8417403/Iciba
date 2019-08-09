cea135ecd94ae845d6d7f5184f9315da97518c9a
xrr8417403 <279484148@qq.com>
2019-08-09 12:39:17 +0800

    update

--- Iciba/spiders/Iciba_spider.py
+++ Iciba/spiders/Iciba_spider.py
@@ -15,8 +15,9 @@ class IcibaSpider(scrapy.Spider):
 
     def parse(self,response):
         for category_url in response.css("div.main_l #mainwordlist > li[has_child]"):
+            header = category_url.css("h3::text").extract_first()
+            study_count = category_url.css("p:nth-child(3)::text").extract_first()
             if category_url.css("::attr(has_child)").extract_first() == '1':
-                header = category_url.css("h3::text").extract_first()
                 for sub_category_url in category_url.css("ol.nobt li[has_child]"):
                     sub_header = sub_category_url.css("a h4::text").extract_first()
                     count = sub_category_url.css("a p:nth-child(3)::text").extract_first()
@@ -24,28 +25,28 @@ class IcibaSpider(scrapy.Spider):
                     url = "http://word.iciba.com/?action=courses&classid=" + str(class_id)
                     #print("<<%s>>%s=======================>%s,url:%s" %(header,sub_header,count,url))
                     yield response.follow(url, callback=self.parse_chapter_url,meta={"header":header,"sub_header":sub_header,\
-                        "count":count})
+                        "count":count,"study_count":study_count})
             else :
-                header = category_url.css("h3::text").extract_first()
                 sub_header = ""
                 count = category_url.css('p::text').extract_first()
                 class_id = category_url.css("::attr(class_id)").extract_first()
                 url = "http://word.iciba.com/?action=courses&classid=" + str(class_id)
                 #print("%s=======================>%s,url::%s"%(header,count,url))
                 yield response.follow(url, callback=self.parse_chapter_url,meta={"header":header,"sub_header":sub_header,\
-                        "count":count})
+                        "count":count,"study_count":study_count})
 
     def parse_chapter_url(self,response):
         for chapter_url in response.css("ul.study-speed-m li.c_panel"):
             header = response.meta["header"]
             sub_header = response.meta["sub_header"]
             count = response.meta["count"]
+            study_count = response.meta["study_count"]
             class_id = response.request.url.split('=')[-1]
             course_id = chapter_url.css("::attr(course_id)").extract_first()
             word_url = "http://word.iciba.com/?action=words&class=" + str(class_id) + "&course=" + str(course_id)
             #print(word_url)
             yield response.follow(word_url, callback=self.parse_word,meta={"header":header,"sub_header":sub_header,\
-                        "count":count})
+                        "count":count,"study_count":study_count})
 
     def parse_word(self,response):
         for info in response.css("ul.word_main_list li"):
@@ -53,8 +54,10 @@ class IcibaSpider(scrapy.Spider):
             'header':response.meta["header"],
             'sub_header':response.meta["sub_header"],
             'count':response.meta["count"],
-            'word':info.css("div.word_main_list_w span::text").extract_first().strip(),
+            "study_count": response.meta["study_count"],
+            'word':info.css("div.word_main_list_w span::attr(title)").extract_first().strip(),
             'phonetic':info.css("div.word_main_list_y strong::text").extract_first().strip(),
             'chinese':info.css("div.word_main_list_s span::attr(title)").extract_first().strip()
+
             #print(header,sub_header,count,word,phonetic,chinese)
             }
